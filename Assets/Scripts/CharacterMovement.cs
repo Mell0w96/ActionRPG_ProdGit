f5f64509fe8ac3581dfc3323375de7fa98155e1d
Marko <mellowofc.business@gmail.com>
2020-01-24 13:34:17 -0500

    Initial commit

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float Speed;
+    public float jumpForce;
+    public float distanceToGround;
+    bool isRunning = false;
+    public Animator anim;
+    public GameObject ground;
+    LayerMask walkable;
+    Rigidbody rb;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = this.gameObject.GetComponent<Animator>();
+        walkable = 1 << LayerMask.NameToLayer("Ground");
+        anim.SetBool("isGrounded", true);
+     
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Debug.Log(Physics.Raycast(this.transform.position, Vector3.down, distanceToGround, walkable));
+        Debug.DrawRay(this.transform.position, Vector3.down, Color.red, distanceToGround);
+
+
+        Vector3 playerMoveVector = new Vector3(horizontal, 0, vertical).normalized * Speed * Time.deltaTime;
+        
+        transform.Translate(playerMoveVector, Space.Self);
+
+        if (isRunning == false)
+        {
+            if (Input.GetButton("Horizontal") == true || Input.GetButton("Vertical") == true)
+            {             
+                isRunning = true;
+                anim.SetBool("isRunning", true);
+            }
+        }
+
+        if (isRunning == true)
+        {
+            if (horizontal == 0 && vertical == 0)
+            {
+                isRunning = false;
+                anim.SetBool("isRunning", false);
+            }
+        }
+
+
+        if (!NotGrounded())
+        {
+           anim.SetBool("isGrounded", true);
+            if (Input.GetButton("Jump"))
+            {
+                anim.SetBool("isGrounded", false);
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+               
+            }
+        }
+        
+       
+       
+
+    }
+
+    bool NotGrounded()
+    {
+      return Physics.Raycast(this.transform.position, Vector3.down, distanceToGround, walkable);
+    }
+    
+}
