29fbd447e665c6e1f7e9fe01e3f586807f9b7607
OverlordVGS <57149179+OverlordVGS@users.noreply.github.com>
2020-01-28 23:56:52 -0500

    Enemy Follow Script
    Created simple follow script where the enemy chases the player.

---
+++ Assets/_ActionRPGAssets/Scripts/EnemyChasePlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyChasePlayer : MonoBehaviour
+{
+
+    float followSpeed = 1f;
+    Transform target;
+
+    // Start is called before the first frame update
+    // Enemy locates the player
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    // Enemy follows the player
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, followSpeed * Time.deltaTime);
+    }
+}
