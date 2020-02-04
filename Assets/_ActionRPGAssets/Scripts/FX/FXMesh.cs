eadc4bf7438e5f8c8db8bc8f53540d85419ea1ae
Dex10k <ddeboone@gmail.com>
2020-02-04 13:14:51 -0500

    Randomised Stalagtite

---
+++ Assets/_ActionRPGAssets/Scripts/FX/FXMesh.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXMesh : MonoBehaviour
+{
+
+    MeshCollider myMeshCollider;
+    MeshFilter myMeshFilter;
+    public Mesh[] PossibleMeshes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myMeshCollider = this.GetComponent<MeshCollider>();
+        myMeshFilter = this.GetComponent<MeshFilter>();
+
+        Mesh CurrentMesh = PossibleMeshes[Mathf.FloorToInt(Random.Range(0,PossibleMeshes.Length))];
+
+        myMeshFilter.mesh = CurrentMesh;
+        myMeshCollider.sharedMesh = CurrentMesh;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
