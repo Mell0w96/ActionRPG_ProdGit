a5c3cb7b167ed00dd5491bf859dc62857ebaf31e
ShadowHaxor99 <chrisfarmer28@gmail.com>
2020-04-09 16:49:52 -0400

    Created Main menu
    Main menu scene created

---
+++ Assets/StartGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void SetScene()
+    {
+        SceneManager.LoadScene("Level2(Final)");
+    }
+}
